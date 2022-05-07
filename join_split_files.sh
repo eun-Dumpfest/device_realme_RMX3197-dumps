ac1c0c672a16be695d447cd7fdd25f2e38b4561e
root <root@fv-az296-623.ie1vkwtfhimuhe55j3he1m2lab.gx.internal.cloudapp.net>
2022-05-07 15:24:32 +0000

    Add extras for sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 0mp3k6833v164P6 release-keys
    Signed-off-by: root <root@fv-az296-623.ie1vkwtfhimuhe55j3he1m2lab.gx.internal.cloudapp.net>

---
+++ join_split_files.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk.* 2>/dev/null >> system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk
+rm -f system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/system_ext/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat my_preload/del-app/com.spotify.music/com.spotify.music.apk.* 2>/dev/null >> my_preload/del-app/com.spotify.music/com.spotify.music.apk
+rm -f my_preload/del-app/com.spotify.music/com.spotify.music.apk.* 2>/dev/null
+cat my_preload/del-app/realme_Link_exp/realmeLink-releaseExp-1.2.258.12_470_50937ab_202101281942_signed.apk.* 2>/dev/null >> my_preload/del-app/realme_Link_exp/realmeLink-releaseExp-1.2.258.12_470_50937ab_202101281942_signed.apk
+rm -f my_preload/del-app/realme_Link_exp/realmeLink-releaseExp-1.2.258.12_470_50937ab_202101281942_signed.apk.* 2>/dev/null
+cat my_preload/del-app/EvenB_2067A_AmazonmusicIN/AmazonMusicIN-17.3.6.0.0.apk.* 2>/dev/null >> my_preload/del-app/EvenB_2067A_AmazonmusicIN/AmazonMusicIN-17.3.6.0.0.apk
+rm -f my_preload/del-app/EvenB_2067A_AmazonmusicIN/AmazonMusicIN-17.3.6.0.0.apk.* 2>/dev/null
+cat my_preload/del-app/in.mohalla.video/in.mohalla.video.apk.* 2>/dev/null >> my_preload/del-app/in.mohalla.video/in.mohalla.video.apk
+rm -f my_preload/del-app/in.mohalla.video/in.mohalla.video.apk.* 2>/dev/null
+cat my_preload/del-app/EvenB_2067A_snapchat/Snapchat_11.6.2.66.apk.* 2>/dev/null >> my_preload/del-app/EvenB_2067A_snapchat/Snapchat_11.6.2.66.apk
+rm -f my_preload/del-app/EvenB_2067A_snapchat/Snapchat_11.6.2.66.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat my_heytap/app/Gmail2/Gmail2.apk.* 2>/dev/null >> my_heytap/app/Gmail2/Gmail2.apk
+rm -f my_heytap/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat my_heytap/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> my_heytap/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f my_heytap/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat my_heytap/app/YouTube/YouTube.apk.* 2>/dev/null >> my_heytap/app/YouTube/YouTube.apk
+rm -f my_heytap/app/YouTube/YouTube.apk.* 2>/dev/null
+cat my_heytap/app/Maps/Maps.apk.* 2>/dev/null >> my_heytap/app/Maps/Maps.apk
+rm -f my_heytap/app/Maps/Maps.apk.* 2>/dev/null
+cat my_heytap/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> my_heytap/app/WebViewGoogle/WebViewGoogle.apk
+rm -f my_heytap/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat my_heytap/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> my_heytap/priv-app/Velvet/Velvet.apk
+rm -f my_heytap/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat my_heytap/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> my_heytap/priv-app/GmsCore/GmsCore.apk
+rm -f my_heytap/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat my_stock/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> my_stock/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f my_stock/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
